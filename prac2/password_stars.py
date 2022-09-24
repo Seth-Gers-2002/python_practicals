5f7e5ffaadf0f6d325afcf3e4970b560432fa6fc
sethg <seth.gersbach@my.jcu.edu.au>
2022-09-24 10:38:58 +1000

    refactor password check program to use functions

--- prac2/password_stars.py
+++ prac2/password_stars.py
@@ -1,7 +1,19 @@
+def main():
+    password = get_password()
 
-password = input("password:")
-while len(password) <= 10:
-    print("invaild")
+    print_censor(password)
+
+
+def print_censor(password):
+    print("*" * len(password))
+
+
+def get_password():
     password = input("password:")
+    while len(password) <= 9:
+        print("invaild")
+        password = input("password:")
+    return password
 
-print("*" * len(password))
+
+main()

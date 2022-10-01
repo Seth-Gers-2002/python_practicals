c1c4bf5ea539585c8347db37d7f20b679e2db93f
sethg <seth.gersbach@my.jcu.edu.au>
2022-10-01 12:47:59 +1000

    Added a loop to check the char in password

--- prac3/password_checker.py
+++ prac3/password_checker.py
@@ -31,15 +31,20 @@ def is_valid_password(password):
     """Determine if the provided password is valid."""
     if (len(password) > 6) or (len(password) < 2):
         return False
-
     count_lower = 0
     count_upper = 0
     count_digit = 0
     count_special = 0
     for char in password:
-        # TODO: count each kind of character (use str methods like isdigit)
-        pass
-
+        if char.isupper():
+            count_upper += 1
+        elif char.islower():
+            count_lower += 1
+        elif char.isdigit():
+            count_digit += 1
+        else:#check
+            count_special += 1
+    print(count_upper, count_lower, count_digit, count_special)
     # TODO: if any of the 'normal' counts are zero, return False
 
     # TODO: if special characters are required, then check the count of those

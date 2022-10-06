f3df21c7a58e259b056a180912bc78b26e89e3d3
sethg <seth.gersbach@my.jcu.edu.au>
2022-10-07 09:59:21 +1000

    Added a function that stores data in nestled list

--- Prac4/subject_reader.py
+++ Prac4/subject_reader.py
@@ -10,8 +10,8 @@ def main():
     data = get_data()
     print(data)
 
-
 def get_data():
+    l = [[]]
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
     for line in input_file:
@@ -19,10 +19,12 @@ def get_data():
         print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
-        parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        l.append(parts)
+        print(l)
+        # print(parts)  # See what the parts look like (notice the integer is a string)
+        # parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
+        # print(parts)  # See if that worked
+        # print("----------")
     input_file.close()
 
 

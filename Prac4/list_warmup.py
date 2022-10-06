763e3301f661d841ce50e707d24c4970967b4868
sethg <seth.gersbach@my.jcu.edu.au>
2022-10-06 15:30:41 +1000

    Added print functions to show results

---
+++ Prac4/list_warmup.py
@@ -0,0 +1,30 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+print(numbers)
+numbers[0] = 10
+print(numbers)
+last_number = len(numbers) - 1
+numbers[last_number] = 1
+print(numbers)
+print(numbers[2:])
+if 9 in numbers:
+    print("9 is in numbers")
+else:
+    print("not in numbers")
+# numbers[0]
+# print 3
+# numbers[-1]
+# print 2
+# numbers[3]
+# print 1
+# numbers[:-1]
+# print 314159
+# numbers[3:4]
+# 1
+# 5 in numbers
+#True
+# 7 in numbers
+# False
+# "3" in numbers
+# False
+# numbers + [6, 5, 3]
+# 3141592653

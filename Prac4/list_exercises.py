1b2c8eb48c65ae2034d33ea265ddb329a7329537
sethg <seth.gersbach@my.jcu.edu.au>
2022-10-07 11:26:31 +1000

    Added functions

--- Prac4/list_exercises.py
+++ Prac4/list_exercises.py
@@ -1,8 +1,33 @@
-
 print('please give me five numbers')
 
+
+def main():
+    get_numbers()
+    print_numbers()
+    numbers_sorted()
+
+
+def numbers_sorted():
+    organised = sorted(numbers)
+    numbers_min = organised[0]
+    numbers_max = organised[4]
+    print(f"numbers_min:{numbers_min}, numbers_max{numbers_max}")
+    average = sum(numbers) / 5
+    print(average)
+
+
+def print_numbers():
+    print(numbers)
+    numbers_first = numbers[0]
+    numbers_last = numbers[4]
+    print(f"first number:{numbers_first}, last number:{numbers_last}")
+
+
+def get_numbers():
+    for i in range(0, 5, 1):
+        number = int(input("number"))
+        numbers.append(number)
+
+
 numbers = []
-for l in range(0,5,1):
-    ah = input(f"number")
-    numbers.append(ah)
-print(numbers)
+main()

b9fe0725b307fc3fa48d1211acca0e04789f8883
sethg <seth.gersbach@my.jcu.edu.au>
2022-10-14 09:57:19 +1000

    Will give colour you ask, not indepedent

--- prac5/hex_colours.py
+++ prac5/hex_colours.py
@@ -1,3 +1,11 @@
 
 hex_colours = {"Absolute Zero": "#0048ba", "Acid Green": "#b0bf1a", "AliceBlue":"#f0f8ff", "Alizarin crimson": "#e32636", "Amaranth":"#e52b50"}
 
+get_colour = input("what colour?")
+
+while get_colour != " ":
+    if get_colour in hex_colours:
+        print(f"{get_colour:{10}} is {hex_colours[get_colour]}")
+    else:
+        print("Invalid colourname")
+    get_colour = input("what colour?")

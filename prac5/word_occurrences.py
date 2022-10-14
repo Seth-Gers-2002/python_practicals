ba8817af472735227d0935ee06cb4d408420d533
sethg <seth.gersbach@my.jcu.edu.au>
2022-10-14 16:49:11 +1000

    Made a loop that filiters through sentences and adds them to a list

--- prac5/word_occurrences.py
+++ prac5/word_occurrences.py
@@ -1,5 +1,16 @@
 """
 word occurrences
-estimated:
+estimated:40
 actual:
 """
+
+stored = []
+sentence = input("sentence: ").split(" ")
+
+for word in sentence:
+    if word not in stored:
+        stored.append(word)
+    else:
+        print("ahh")
+
+print(stored)

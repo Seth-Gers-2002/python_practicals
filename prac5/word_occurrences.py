efd6330663f799c5d7a776cd8de55cfa7aa6f2b3
sethg <seth.gersbach@my.jcu.edu.au>
2022-10-14 18:02:15 +1000

    It prints in alphabtical order, one at a time

--- prac5/word_occurrences.py
+++ prac5/word_occurrences.py
@@ -4,12 +4,15 @@ estimated:35
 actual:
 """
 
-stored = {}
+number_of_letters = {}
 sentence = input("sentence: ").split(" ")
 
 for word in sentence:
-    if word not in stored:
-        stored.update({word:int(1)})
+    if word not in number_of_letters:
+        number_of_letters.update({word:int(1)})
     else:
-        stored[word] = stored.get(word) + 1
-print(sorted(stored.items()))
+        number_of_letters[word] = number_of_letters.get(word) + 1
+
+sorted_number_of_letters = (sorted(number_of_letters.items()))
+for words in sorted_number_of_letters:
+    print(words)

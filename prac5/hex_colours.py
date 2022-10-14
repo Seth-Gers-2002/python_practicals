c2e195c11451c5a247f0104165d91f278a7a3245
sethg <seth.gersbach@my.jcu.edu.au>
2022-10-14 11:26:01 +1000

    Re-arrange the variables in the dictionary to make it easier to find no matter the input

--- prac5/hex_colours.py
+++ prac5/hex_colours.py
@@ -1,11 +1,14 @@
 
-hex_colours = {"Absolute Zero": "#0048ba", "Acid Green": "#b0bf1a", "AliceBlue":"#f0f8ff", "Alizarin crimson": "#e32636", "Amaranth":"#e52b50"}
+hex_colours = {"absolutezero": "#0048ba", "acidgreen": "#b0bf1a", "aliceblue":"#f0f8ff", "alizarincrimson": "#e32636", "amaranth":"#e52b50"}
 
 get_colour = input("what colour?")
+correct_get_colour = get_colour.lower().replace(" ","")
 
-while get_colour != " ":
-    if get_colour in hex_colours:
-        print(f"{get_colour:{10}} is {hex_colours[get_colour]}")
+
+while get_colour != "":
+    if correct_get_colour in hex_colours:
+        print(f"{get_colour:{10}} is {hex_colours[correct_get_colour]}")
     else:
         print("Invalid colourname")
     get_colour = input("what colour?")
+    correct_get_colour = get_colour.lower()

ee3dc9e46a76fa03135d4c942da98f303188e867
sethg <seth.gersbach@my.jcu.edu.au>
2022-10-14 15:24:37 +1000

    Add more variable to the dictionary

--- prac5/hex_colours.py
+++ prac5/hex_colours.py
@@ -1,14 +1,13 @@
-
-hex_colours = {"absolutezero": "#0048ba", "acidgreen": "#b0bf1a", "aliceblue":"#f0f8ff", "alizarincrimson": "#e32636", "amaranth":"#e52b50"}
+hex_colours = {"absolutezero": "#0048ba", "acidgreen": "#b0bf1a", "aliceblue": "#f0f8ff", "alizarincrimson": "#e32636",
+               "amaranth": "#e52b50", "amber": "#ffbf00", "amethyst": "#9966cc", "antiquewhite": "#faebd7"}
 
 get_colour = input("what colour?")
-correct_get_colour = get_colour.lower().replace(" ","")
-
+correct_get_colour = get_colour.lower().replace(" ", "")
 
 while get_colour != "":
     if correct_get_colour in hex_colours:
         print(f"{get_colour:{10}} is {hex_colours[correct_get_colour]}")
     else:
-        print("Invalid colourname")
+        print("Invalid colour-name")
     get_colour = input("what colour?")
     correct_get_colour = get_colour.lower()

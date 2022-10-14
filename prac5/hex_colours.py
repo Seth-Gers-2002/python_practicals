0949ac7f1296a86223d9578bec657d0353009772
sethg <seth.gersbach@my.jcu.edu.au>
2022-10-14 16:15:24 +1000

    added the replace functions to the inputs to remove gaps

--- prac5/hex_colours.py
+++ prac5/hex_colours.py
@@ -6,8 +6,8 @@ correct_get_colour = get_colour.lower().replace(" ", "")
 
 while get_colour != "":
     if correct_get_colour in hex_colours:
-        print(f"{get_colour:{10}} is {hex_colours[correct_get_colour]}")
+        print(f"{get_colour} is {hex_colours[correct_get_colour]}")
     else:
         print("Invalid colour-name")
     get_colour = input("what colour?")
-    correct_get_colour = get_colour.lower()
+    correct_get_colour = get_colour.lower().replace(" ", "")

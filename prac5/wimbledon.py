f1e6caa50d9094bc7cde38d5ec89cf8784a04c44
sethg <seth.gersbach@my.jcu.edu.au>
2022-10-17 17:06:31 +1000

    Functions stores the information and returns it

--- prac5/wimbledon.py
+++ prac5/wimbledon.py
@@ -1,6 +1,19 @@
+"""
+the champions and how many times they have won.
+the countries of the champions in alphabetical order
+"""
 
-out_file = open("wimbledon.csv")
-for line in out_file:
-    print(line)
+def get_information():
+    stored = []
+    with open("wimbledon.csv", "r", encoding="utf-8-sig") as in_file:
+        in_file.readline()
+        for line in in_file:
+            line = line.strip()
+            parts = line.split(',')
+            stored.append(parts)
+    in_file.close()
+    return stored
 
-out_file.close()
+
+
+get_information()

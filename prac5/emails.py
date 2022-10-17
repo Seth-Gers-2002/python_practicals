f80de4c03a8ef9b7e2c22d190af1a62ecd4c1c39
sethg <seth.gersbach@my.jcu.edu.au>
2022-10-17 18:48:10 +1000

    remormatted the files

--- prac5/emails.py
+++ prac5/emails.py
@@ -1,9 +1,35 @@
+"""
+come back and figure out why we need join
 
-email = input("Email: ")
+"""
 
-email_split = email.split("@")
-if "." in email_split[0]:
-    name = email_split[0].split(".")
-    print(f"is your name {name[0]},{name[1]}")
-else:
-    print(email_split)
+
+def check_if_name_is_correct(name):
+    name_check = input("(Y/n):")
+    if name_check == "Y" or name_check == "y" or name_check == "yes" or name_check == "Yes":
+        return name
+    elif name_check == "N" or name_check == "n" or name_check == "no" or name_check == "No":
+        name_correct = input("So what is your name? ").title()
+        return name_correct
+
+
+def main():
+    email_to_name = {}
+    email = input("Email: ")
+    email_split = email.split("@")
+    while email != "":
+        if "." in email_split[0]:
+            name = email_split[0].replace(".", " ").title()
+        else:
+            name = email_split[0].title()
+        print(f"is your name {name}?")
+        name_checked = check_if_name_is_correct(name)
+        email_to_name[email] = name_checked
+
+        email = input("Email: ")
+        email_split = email.split("@")
+    for email, name in email_to_name.items():
+        print(f"{name} ({email})")
+
+
+main()

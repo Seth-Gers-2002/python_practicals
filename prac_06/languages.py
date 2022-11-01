ac466378e74aa31314f6d71e4950865a526d8992
sethg <seth.gersbach@my.jcu.edu.au>
2022-11-01 19:51:35 +1000

    Added a loop that checks to see if dynamic

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -5,7 +5,15 @@ def main():
     python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
     ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
     visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
-    print(python, ruby, visual_basic)
+    print(python)
+    print(ruby)
+    print(visual_basic)
+
+    languages = [python, ruby, visual_basic]
+
+    for language in languages:
+        if language.is_dynamic():
+            print(f"{language.field} is dynamic")
 
 main()
 

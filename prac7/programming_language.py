9433c1e8700942ee7b12fcec2a591fdb4ee16e97
sethg <seth.gersbach@my.jcu.edu.au>
2022-11-05 00:55:54 +1000

    Added pointer arithmetic to display and collection

---
+++ prac7/programming_language.py
@@ -0,0 +1,49 @@
+"""
+CP1404/CP5632 Practical - Suggested Solution
+Programming Language class with tests.
+"""
+
+
+class ProgrammingLanguage:
+    """Represent information about a programming language."""
+
+    def __init__(self, name, typing, reflection, year, pointer_arithmetic):
+        """Construct a ProgrammingLanguage from the given values."""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+        self.Pointer_arithmetic = pointer_arithmetic
+
+    def __repr__(self):
+        """Return string representation of a ProgrammingLanguage."""
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}, Pointer Arithmetic({self.Pointer_arithmetic})"
+
+    def is_dynamic(self):
+        """Determine if language is dynamically typed."""
+        return self.typing == "Dynamic"
+
+    def pointer_arithmetic(self):
+        if self.pointer_arithmetic() == "No":
+            return False
+        else:
+            return True
+
+
+def run_tests():
+    """Run simple tests/demos on ProgrammingLanguage class."""
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995, "No")
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991, "No")
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991, "oo")
+
+    languages = [ruby, python, visual_basic]
+    print(python)
+
+    print("The dynamically typed languages are:")
+    for language in languages:
+        if language.is_dynamic():
+            print(language.name)
+
+    print(ruby.Pointer_arithmetic)
+if __name__ == "__main__":
+    run_tests()

fb3b0d635cc3e773d4b67c70ed7ce733f4fd5e6d
sethg <seth.gersbach@my.jcu.edu.au>
2022-11-22 15:19:26 +1000

    Runs expected test

---
+++ prac9/taxi_test.py
@@ -0,0 +1,13 @@
+from prac9.taxi import Taxi
+
+
+def main():
+    my_car = Taxi("prius 1 ", 100, 1.23)
+    my_car.drive(40)
+    print(my_car)
+    my_car.start_fare()
+    my_car.drive(100)
+    print(my_car)
+
+
+main()
